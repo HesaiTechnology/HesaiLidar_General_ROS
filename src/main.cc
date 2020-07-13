674d8973233b121b2cfe965548b5c55bc2594884
wuxiaozhou <wuxiaozhou@hesaitech.com>
2020-07-13 18:14:46 +0800

    Delete unused include

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/io/pcd_io.h>
