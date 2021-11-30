b9605152b9aa6e0e2d5b1f6f896fe2c2c6eeb568
dadaroce <danielcediel97@gmail.com>
2021-11-30 10:37:26 -0500

    [FEAT]: update launch file

--- launch/hesai_lidar_launch.py
+++ launch/hesai_lidar_launch.py
@@ -6,12 +6,12 @@ def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
             package ='hesai_lidar',
-            node_namespace ='hesai',
-            node_executable ='hesai_lidar_node',
+            namespace ='hesai',
+            executable ='hesai_lidar_node',
             name ='hesai_node',
             output ="screen",
             parameters=[
-                {"pcap_file": "''"},
+                {"pcap_file": ""},
                 {"server_ip"  : "192.168.1.201"},
                 {"lidar_recv_port"  : 2368},
                 {"gps_port"  : 10110},
@@ -20,13 +20,13 @@ def generate_launch_description():
                 {"frame_id"  : "PandarXT-16"},
                 {"pcldata_type"  : 0},
                 {"publish_type"  : "both"},
-                {"timestamp_type"  : "''"},
-                {"data_type"  : "''"},
-                {"lidar_correction_file"  : "./src/ROS/HesaiLidar_General_ROS/config/PandarXT-16.csv"},
-                {"multicast_ip"  : "''"},
+                {"timestamp_type"  : ""},
+                {"data_type"  : ""},
+                {"lidar_correction_file"  : "config/PandarXT-32.csv"},
+                {"multicast_ip"  : ""},
                 {"coordinate_correction_flag"  : False},
-                {"fixed_frame"  : "''"},
-                {"target_frame_frame"  : "''"}
+                {"fixed_frame"  : ""},
+                {"target_frame_frame"  : ""}
             ]
         )
     ])
