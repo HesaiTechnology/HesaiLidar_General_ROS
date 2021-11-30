fc822f19d92ed9d51cddcf0466eba532a43fb72d
dadaroce <danielcediel97@gmail.com>
2021-11-30 11:04:57 -0500

    [FEAT]: hesai config file as absolute

--- launch/hesai_lidar_launch.py
+++ launch/hesai_lidar_launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 import launch_ros.actions
+from os import path
 
 
 def generate_launch_description():
@@ -12,17 +13,17 @@ def generate_launch_description():
             output ="screen",
             parameters=[
                 {"pcap_file": ""},
-                {"server_ip"  : "192.168.1.201"},
+                {"server_ip"  : "192.168.1.100"},
                 {"lidar_recv_port"  : 2368},
                 {"gps_port"  : 10110},
                 {"start_angle"  : 0.0},
-                {"lidar_type"  : "PandarXT-16"},
-                {"frame_id"  : "PandarXT-16"},
+                {"lidar_type"  : "PandarXT-32"},
+                {"frame_id"  : "PandarXT-32"},
                 {"pcldata_type"  : 0},
                 {"publish_type"  : "both"},
                 {"timestamp_type"  : ""},
                 {"data_type"  : ""},
-                {"lidar_correction_file"  : "config/PandarXT-32.csv"},
+                {"lidar_correction_file"  : path.dirname(path.abspath(__file__))+"/../config/PandarXT-32.csv"},
                 {"multicast_ip"  : ""},
                 {"coordinate_correction_flag"  : False},
                 {"fixed_frame"  : ""},
